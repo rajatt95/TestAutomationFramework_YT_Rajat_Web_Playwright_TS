b0f561b6daa380316a029d6f14efeef628a87a10
“Rajat <rajatvermaa95@gmail.com>
2025-04-27 00:04:59 +0530

    #15: ⚙️ Configurations (Retries, Videos, Traces, Screenshots, Workers)

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,10 +18,15 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  // retries: process.env.CI ? 2 : 0,
+  retries: 1, // Give failing tests 1 retry attempt(s)  
+  
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  // workers: process.env.CI ? 1 : undefined,
+  workers:6,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -31,7 +36,14 @@ export default defineConfig({
     // BASE_URL=https://www-prod.saucedemo.com npx playwright test
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    // trace: 'on-first-retry',
+    trace: 'on',
+
+    // video: 'retain-on-failure',
+    video: 'on',
+
+    // screenshot: 'only-on-failure',
+    screenshot: 'on',
   },
 
   /* Configure projects for major browsers */
